cf386adce283ed1b2775c121045a6baae43c30ae
miragzamov <miragzamov24@gmail.com>
2025-03-27 16:15:04 +0500

    remove confetti effect

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 import "https://esm.run/@material/web/all.js";
-import "https://cdn.jsdelivr.net/npm/canvas-confetti@1.6.0";
 
 window.onload = function() {
   document.getElementById('loadingScreen').style.opacity = '0';
@@ -19,15 +18,3 @@ infoButton.addEventListener("click", () => {
 infoDialogOkButton.addEventListener("click", () => {
   infoDialog.close();
 });
-
-const iconElement = document.querySelector(".exam-icon");
-
-iconElement.addEventListener("click", launchConfetti);
-
-function launchConfetti() {
-  confetti({
-    particleCount: 500,
-    spread: 70,
-    origin: { y: 0.6 }
-  });
-}
